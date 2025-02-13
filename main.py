e9392792f69bf720613c0443a92baa1e5644ed58
Rafael Gonçalves <jrafaelg@live.com>
2025-02-13 16:07:17 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    t = int(input("quantos caracteres na senha? "))
+    ma = input("letras maíusculas (S/N)? ").upper()
+    mi = input("letras minúsculas (S/N)? ").upper()
+    di = input("digitos (S/N)? ").upper()
+    si = input("Simbolos (S/N)? ").upper()
+    rc = input("Remover simbolos que podem confundir (S/N)? ").upper()
